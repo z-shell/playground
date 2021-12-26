ddbc73bb4a5a511b380951ae87d3c9906451ff84
Salvydas Lukosius <sall@w-ss.io>
2021-12-26 09:23:19 +0000

    Update bootstrap.sh

--- NICHOLAS85/bootstrap.sh
+++ NICHOLAS85/bootstrap.sh
@@ -2,6 +2,7 @@
 
 curl 'https://sh.rustup.rs' -sSf | sh -s -- -y
 echo 'source ${HOME}/.cargo/env' >> ${HOME}/.zshenv
-    
+source $HOME/.cargo/env
+
 sudo apt install --yes tlp lscolors fd-find fzf
 cargo install lsd
